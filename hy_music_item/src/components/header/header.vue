ce140eda61739d5b78383516bee263e546225041
张雨晨 <15596686748@163.com>
2022-11-01 21:30:52 +0800

    hy

---
+++ hy_music_item/src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+    <span class="icon"></span>
+    <h1 class="text"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "m-header",
+};
+</script>
+
+<style lang="scss" scoped></style>
