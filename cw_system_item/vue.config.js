6a31f8d50002524095795301e0495f8c63ebaa61
张雨晨 <15596686748@163.com>
2022-11-09 00:36:15 +0800

    项目架构

--- cw_system_item/vue.config.js
+++ cw_system_item/vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+const { resolve } = require('path')
+
+module.exports = {
+  outputDir: './build',
+  devServer: {},
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    },
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
+}
