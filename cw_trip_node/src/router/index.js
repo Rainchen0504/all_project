009e28eb256a3b27b154d57d6a0e1d7b2ab2932a
张雨晨 <15596686748@163.com>
2022-11-08 20:54:20 +0800

    路由修改格式

--- cw_trip_node/src/router/index.js
+++ cw_trip_node/src/router/index.js
@@ -1,16 +1,15 @@
-const fs = require("fs")
+const fs = require("fs");
 
 //readdirSync同步返回结果
 //readdir异步回调返回结果
-const useRoutes = function(){
-    fs.readdirSync("./src/router").forEach((item) => {
-        if(item === "index.js") return 
-        const router = require(`./${item}`)
-        //启动路由
-        this.use(router.routes())
-        this.use(router.allowedMethods());
-    })
-}
+const useRoutes = function () {
+  fs.readdirSync("./src/router").forEach((item) => {
+    if (item === "index.js") return;
+    const router = require(`./${item}`);
+    //启动路由
+    this.use(router.routes());
+    this.use(router.allowedMethods());
+  });
+};
 
-
-module.exports = useRoutes
+module.exports = useRoutes;
