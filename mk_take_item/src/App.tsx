5dfa0609a3a81786d41cd71178cd8d1754cdc578
张雨晨 <15596686748@163.com>
2022-11-14 00:03:06 +0800

    提交内容

--- mk_take_item/src/App.tsx
+++ mk_take_item/src/App.tsx
@@ -1,24 +1,57 @@
-import { defineComponent, ref } from 'vue'
-import Add from './Add'
+import { defineComponent, Ref, ref } from 'vue'
+
+// 引入开源项目，用js写css
+import { createUseStyles } from 'vue-jss'
+
+//引入用monaco写的组件
+import MonacoEditor from './components/MonacoEditor'
+
+const toJson = (params: any) => {
+  //保留格式的json转换
+  return JSON.stringify(params, null, 2)
+}
+
+const schema = {
+  // schema规则
+  type: 'string',
+}
+
+const useStyles = createUseStyles({
+  // 写样式
+  editor: {
+    minHeight: 400,
+  },
+})
 
 // h函数开发
-const App = defineComponent({
+export default defineComponent({
   setup() {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const addFunc = ref(() => {})
-    const getChildFunc = (add: any) => {
-      addFunc.value = add
-      console.log('获取子组件报漏的方法', addFunc)
+    //获取响应式的schema
+    const schemaRef: Ref<any> = ref(schema)
+
+    const handleCodeChange = (code: string) => {
+      // 当在页面修改了code后会触发
+      let schema: any
+      try {
+        schema = JSON.parse(code)
+      } catch (e) {
+        schemaRef.value = schema
+      }
     }
+
+    const classesRef = useStyles()
+    const classes = classesRef.value
+    const code = toJson(schemaRef.value)
     return () => (
-      <div>
-        父组件
-        <Add onLoad={getChildFunc} />
-        <hr></hr>
-        <button onClick={addFunc.value}>父组件调用子组件方法</button>
+      <div id="app">
+        <div>这是首页</div>
+        <MonacoEditor
+          code={code}
+          onChange={handleCodeChange}
+          title="Schema"
+          class={classes.editor}
+        />
       </div>
     )
   },
 })
-
-export default App
--- mk_take_item/src/main.ts
+++ mk_take_item/src/main.ts
@@ -1,26 +1,4 @@
-import { createApp, defineComponent, createVNode, h, ref } from 'vue'
-// import App from './App.vue'
+import { createApp } from 'vue'
 import App from './App'
-
-// import son_com from '../src/components/SonCom.vue'
-// h函数开发
-// const App = defineComponent({
-//   setup() {
-//     const numbeef = ref(1)
-//     const name = ref('张雨晨')
-
-//     //在这里声明变量会因为闭包的特性在return中任意位置访问到
-//     return () => {
-//       const number = numbeef.value
-//       return h('div', { id: 'app' }, [
-//         '哈哈哈',
-//         createVNode(son_com, {
-//           name: name.value,
-//           age: number,
-//         }),
-//       ])
-//     }
-//   },
-// })
 
 createApp(App).mount('#app')
